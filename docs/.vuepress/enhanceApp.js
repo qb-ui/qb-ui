6adaf27ae6c025946849b06f3dee545ed65c2fe5
maqingbo <wmaqingbo@163.com>
2019-05-11 19:05:09 +0800

    docs: 修改引用的文件路径

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,8 +4,8 @@
 import VueHighlight from 'vue-highlight.js'
 import 'vue-highlight.js/lib/allLanguages'
 import 'highlight.js/styles/atom-one-dark.css'
-import qbUI from '../../packages'
-import '../../packages/theme-default/lib/index.css'
+import qbUI from '../../src'
+import '../../src/packages/theme-default/lib/index.css'
 import VueClipboard from 'vue-clipboard2'
 
 export default ({
